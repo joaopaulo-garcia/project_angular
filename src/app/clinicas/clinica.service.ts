f8a31272ac87950d70c895adfd11c20cea6ca5ed
Joao Paulo Garcia <joaopaulo.oficial92@gmail.com>
2023-06-12 21:26:34 -0300

    feito

---
+++ src/app/clinicas/clinica.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http'
+import { Clinica } from "./clinica";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class ClinicaService {
+
+    constructor(private http: HttpClient) { }
+
+    private apiURL : string = 'http://localhost:8080/';
+
+    listarClinicas() : Observable<Clinica[]> {
+        
+        return this.http.get<Clinica[]>(this.apiURL + "clinicas");
+    }
+
+    listarPorId(id: number) : Observable<Clinica> {
+
+         // http://localhost:8080/clinicas/id
+        return this.http.get<Clinica>(this.apiURL + "clinicas/" + id);
+    }
+
+    atualizarClinica(id: number, clinica: any) : Observable<Clinica> {
+        return this.http.put<Clinica>(this.apiURL + "clinicas/" + id, clinica);
+    }
+}
+
+
+
+
+
+
+    // @Autowired
+
+    // Java
+    // somar( int numero1 )
+    
+    // Angular
+    // somar( numero1 int ) 
---
+++ src/app/clinicas/lista-clinica/lista-clinica.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Clinica } from '../clinica';
+import { ClinicaService } from '../clinica.service';
+
+@Component({
+  selector: 'app-lista-clinica',
+  templateUrl: './lista-clinica.component.html',
+  styleUrls: ['./lista-clinica.component.css']
+})
+export class ListaClinicaComponent implements OnInit {
+
+  public clinicas!: Clinica[];
+
+  constructor(private clinicaService: ClinicaService) { }
+
+  ngOnInit(): void {
+
+    this.clinicaService.listarClinicas()
+
+      .subscribe(clinicasRecebidas => {
+
+        this.clinicas = clinicasRecebidas
+      },
+        erro => console.log(erro)
+      )
+
+  }
+
+}
